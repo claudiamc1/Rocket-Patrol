e0be29e0fba85a5e06afd16fb35e894914998c16
claudiamc1 <ccmcmill@ucsc.edu>
2021-06-25 23:11:06 -0700

    added explode animation

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,6 +7,7 @@ class Play extends Phaser.Scene {
         this.load.image('rocket', './assets/rocket.png');
         this.load.image('spaceship', './assets/spaceship.png');
         this.load.image('starfield', './assets/starfield.png');
+        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
     }
 
     create() {
@@ -27,6 +28,12 @@ class Play extends Phaser.Scene {
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
+        this.anims.create({
+            key: 'explode',
+            frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 9, first: 0}),
+            frameRate: 30
+        });
     }
 
     update() {
@@ -37,15 +44,15 @@ class Play extends Phaser.Scene {
         this.ship03.update();
         if(this.checkCollision(this.p1Rocket, this.ship03)) {
             this.p1Rocket.reset();
-            this.ship03.reset();
+            this.shipExplode(this.ship03);
           }
           if (this.checkCollision(this.p1Rocket, this.ship02)) {
             this.p1Rocket.reset();
-            this.ship02.reset();
+            this.shipExplode(this.ship02);
           }
           if (this.checkCollision(this.p1Rocket, this.ship01)) {
             this.p1Rocket.reset();
-            this.ship01.reset();
+            this.shipExplode(this.ship01);
           }
     }
 
@@ -59,4 +66,15 @@ class Play extends Phaser.Scene {
                 return false;
             }
     }
+
+    shipExplode(ship) {
+        ship.alpha = 0;
+        let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0, 0);
+        boom.anims.play('explode');             
+        boom.on('animationcomplete', () => {    
+          ship.reset();                        
+          ship.alpha = 1;                       
+          boom.destroy();                       
+        });       
+      }
 }
