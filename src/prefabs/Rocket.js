634ffb60028652a44a4e2945bb051fa9bbd7f541
claudiamc1 <ccmcmill@ucsc.edu>
2021-06-25 22:48:52 -0700

    ships and rocket now reset

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,7 +1,7 @@
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
-        
+
         scene.add.existing(this);
         this.isFiring = false;
         this.moveSpeed = 2;
@@ -22,8 +22,12 @@ class Rocket extends Phaser.GameObjects.Sprite {
             this.y -= this.moveSpeed;
         }
         if(this.y <= borderUISize * 3 + borderPadding) {
-            this.isFiring = false;
-            this.y = game.config.height - borderUISize - borderPadding;
+            this.reset();
         }
+    }
+
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
     }
 }
