51ee8fc7ef4acc476a96382612c7f3b0870fca90
claudiamc1 <ccmcmill@ucsc.edu>
2021-06-29 00:44:51 -0700

    added new assets

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -7,30 +7,14 @@ class Menu extends Phaser.Scene {
         this.load.audio('sfx_select', './assets/blip_select.wav');
         this.load.audio('sfx_explosion', './assets/explosion.wav');
         this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+        this.load.image('title', './assets/title_screen.png');
     }
 
     create() {
-        let menuConfig = {
-            fontFamily: 'Courier',
-            fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
-            align: 'right',
-            padding: {
-              top: 5,
-              bottom: 5,
-            },
-            fixedWidth: 0
-          }
+        this.title = this.add.tileSprite(0, 0, 640, 480, 'title').setOrigin(0, 0);
 
-          this.add.text(game.config.width / 2, game.config.height / 2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
-          this.add.text(game.config.width / 2, game.config.height / 2, 'Use <- -> arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
-          menuConfig.backgroundColor = '#00FF00';
-          menuConfig.color = '#000';
-          this.add.text(game.config.width / 2, game.config.height / 2 + borderUISize + borderPadding, 'Press <- for Novice or -> for Expert', menuConfig).setOrigin(0.5);
-
-          keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-          keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
 
     update() {
