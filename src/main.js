b64ef8a4e9143411a69fa9441bf97c60afb84a3d
claudiamc1 <ccmcmill@ucsc.edu>
2021-06-29 21:40:17 -0700

    commented project information

--- src/main.js
+++ src/main.js
@@ -10,3 +10,14 @@ let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
 let keyF, keyR, keyLEFT, keyRIGHT;
+
+/*Claudia McMillin, Sheep Patrol, 6.29.21, approx. 7 hours
+Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
+Implement parallax scrolling (10)
+Create 4 new explosion SFX and randomize which one plays on impact (10)
++ automatic 20 points from finishing tutorial
+
+sources:
+parallax scrolling: https://www.joshmorony.com/how-to-create-a-parallax-background-in-phaser/
+randomize sfx: https://www.html5gamedevs.com/topic/37506-pick-random-element/
+sfx: https://mixkit.co/free-sound-effects/game/, https://www.fesliyanstudios.com/royalty-free-sound-effects-download/lamb-sheep-260*/
